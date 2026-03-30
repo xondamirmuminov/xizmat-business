6b367d9990fed6088b2ee781df46c64153ac1c4c
xondamirmuminov <xondamirmuminov10@gmail.com>
2026-03-30 15:36:31 +0500

    feat: add unistyles

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ["babel-preset-expo"],
+
+    plugins: [
+      [
+        "react-native-unistyles/plugin",
+        {
+          root: "src",
+          debug: true,
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
+  };
+};
